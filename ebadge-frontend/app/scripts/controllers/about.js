9e32fa2c1c7b3dc60cdda8ea1040e06e5e75939c
Ruben Romero Montes <rubenrm1@gmail.com>
2016-10-10 18:55:41 +0200

    frontend skeleton from yo angular generator

---
+++ ebadge-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ebadgeFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ebadgeFrontendApp
+ */
+angular.module('ebadgeFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ebadge-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ebadgeFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ebadgeFrontendApp
+ */
+angular.module('ebadgeFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
