b395cd0f5e5bb2acee47e6a83c8e3f6a749e7e10
Olivier Mungo <olivier.mungo@ec.europa.eu>
2016-10-15 12:52:42 +0200

    no message

--- ebadge-frontend/app/scripts/helpers/backEnd/backEnd.service.js
+++ ebadge-frontend/app/scripts/helpers/backEnd/backEnd.service.js
@@ -4,50 +4,12 @@ angular.module('ebadgeFrontendApp')
 .factory('backEnd', function ($rootScope, $http, $q, backEndUrl) {
   $http.defaults.headers.common['Content-Type'] = 'application/json';
 
-  function _login (email, password) {
-    var deferred = $q.defer();
-
-    // Authenticate
-    _call('POST', 'authenticate', { email: email, password: password }).then(
-      function (result) {
-        if (result.hasOwnProperty('authToken') && result.authToken.hasOwnProperty('token') && result.hasOwnProperty('authUser')) {
-          $http.defaults.headers.common['X-Auth-Token'] = result.authToken.token;
-
-          deferred.resolve(result.authUser);
-        }
-        else {
-          deferred.reject('User not found');
-        }
-      },
-      function (error) {
-        deferred.reject(error);
-      }
-    );
-
-    return deferred.promise;
-  }
-
-  function _logout () {
-    var deferred = $q.defer();
-
-    _call('POST', 'signout').then(
-      function () {
-        $http.defaults.headers.common['X-Auth-Token'] = undefined;
-        deferred.resolve();
-      },
-      function (error) {
-        deferred.reject(error);
-      }
-    );
-
-    return deferred.promise;
-  }
-
   function _call (method, url, data) {
     var deferred = $q.defer();
 
     $http({ method: method, url: backEndUrl+url, data: data })
       .success(function (result) {
+        console.log(result);
         deferred.resolve(result);
       })
       .error(function (error) {
@@ -58,8 +20,6 @@ angular.module('ebadgeFrontendApp')
   }
 
   return {
-    login: _login,
-    logout: _logout,
     call: _call
   };
 });
