ed466e90bf8ebef50334e9b8ca4769779ab17bec
andima <antdim@gmail.com>
2016-10-15 12:55:46 +0200

    first QR code generation on back-end, to further improve

--- ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/VisitorResource.java
+++ ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/VisitorResource.java
@@ -1,5 +1,10 @@
 package eu.europa.ec.interhack.ebadge.resource;
 
+import java.awt.Color;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import eu.europa.ec.interhack.ebadge.dto.VisitorResponse;
 import eu.europa.ec.interhack.ebadge.model.Visitor;
+import eu.europa.ec.interhack.ebadge.qr.encode.Encoder;
+import eu.europa.ec.interhack.ebadge.qr.encode.EncodingException;
+import eu.europa.ec.interhack.ebadge.qr.encode.EncodingOptions;
+import eu.europa.ec.interhack.ebadge.qr.encode.EncodingOptions.ImageType;
 import eu.europa.ec.interhack.ebadge.repository.VisitorRepository;
 
 /**
@@ -17,6 +26,8 @@ import eu.europa.ec.interhack.ebadge.repository.VisitorRepository;
  */
 @RestController
 public class VisitorResource {
+
+	private static final String QRCODE_FOLDER = System.getProperty("user.dir");
 
 	@Autowired
 	private VisitorRepository repo;
@@ -33,11 +44,36 @@ public class VisitorResource {
 		return new VisitorResponse("OK");
 	}
 
+	@CrossOrigin(value = "*")
 	@RequestMapping(value = "/check", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public VisitorResponse checkVisitor(@RequestParam(value = "visitor") Visitor visitor) {
 
 		// TODO here check an already registered visitor
 
-		return new VisitorResponse("OK");
+		// TODO query the db to check whether the user exists or not
+		// further checks
+
+		boolean validationOk = true;
+
+		if (validationOk) {
+
+			// crate the qr-code UPON VALIDATION
+			EncodingOptions options = new EncodingOptions();
+			options.setSize(340);
+			options.setImageType(ImageType.PNG);
+			options.setMargin(1);
+			options.setForegroundColor(Color.BLACK);
+			String name = "" + new Random().nextInt(99);
+
+			try {
+				File out = Encoder.encode(QRCODE_FOLDER, name, visitor.getId(), options);
+			} catch (EncodingException e) {
+				e.printStackTrace();
+			}
+
+			return new VisitorResponse("OK");
+		}
+		
+		return new VisitorResponse("NOK");
 	}
 }
