c8d029cf3bb037423446e8fc9565b86eb96dedea
andima <antdim@gmail.com>
2016-10-15 13:40:33 +0200

    added mail producer

--- ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/MailSender.java
+++ ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/MailSender.java
@@ -22,7 +22,7 @@ public class MailSender {
 		props.put("mail.smtp.port", "465");
 	}
 	
-	public void sendEmail() {
+	public void sendEmail(String toEmail) {
 		System.out.println("preparing email...");
 		
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
@@ -32,19 +32,19 @@ public class MailSender {
 		});
 
 		try {
-
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("from-email@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("to-email@gmail.com"));
-			message.setSubject("Testing Subject");
+			message.setFrom(new InternetAddress("ebadge-interhack@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+			message.setSubject("Ebadge - InterHack: Your eBadge is ready");
 			message.setText("Dear Mail Crawler," + "\n\n No spam to my email, please!");
 
 			System.out.println("sending email...");
 			Transport.send(message);
 
-			System.out.println("Done");
+			System.out.println("Email sent successfully");
 
 		} catch (MessagingException e) {
+			System.out.println("Something went wrong while trying to send the email");
 			throw new RuntimeException(e);
 		}
 	}
