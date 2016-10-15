94eab7379e9e34e504db92e3c39e6a6ec01f1477
andima <antdim@gmail.com>
2016-10-15 11:02:41 +0200

    Visitor pojo

--- ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/HelloResource.java
+++ ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/HelloResource.java
@@ -1,8 +1,5 @@
 package eu.europa.ec.interhack.ebadge.resource;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
