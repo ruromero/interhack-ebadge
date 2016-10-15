0119a230e3b8ef641ffe6d11464d6e35b91e2067
andima <antdim@gmail.com>
2016-10-15 11:59:11 +0200

    comment out for the moment the error handler, fixed registration handler

--- ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/RegisterVisitor.java
+++ ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/RegisterVisitor.java
@@ -1,9 +1,9 @@
 package eu.europa.ec.interhack.ebadge.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.europa.ec.interhack.ebadge.model.Visitor;
@@ -18,8 +18,8 @@ public class RegisterVisitor {
 	@Autowired
 	private VisitorRepository repo;
 	
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(@RequestParam(value="visitor") Visitor visitor) {
+    @RequestMapping(value = "/register", method = RequestMethod.POST, consumes = "application/json")
+    public String register(@RequestBody Visitor visitor) {
     	
     	// blindly save it to the db for the moment
     	repo.insert(visitor);
