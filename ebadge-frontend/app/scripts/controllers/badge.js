711382d5cc6e2dcacd76113710a6d6566032eba5
Ruben Romero Montes <rubenrm1@gmail.com>
2016-10-15 16:06:09 +0200

    badge page prefilled

--- ebadge-frontend/app/scripts/controllers/badge.js
+++ ebadge-frontend/app/scripts/controllers/badge.js
@@ -8,5 +8,19 @@
  * Controller of the ebadgeFrontendApp
  */
 angular.module('ebadgeFrontendApp')
-  .controller('BadgeCtrl', function () {
+  .controller('BadgeCtrl', function ($location) {
+
+  	this.model = {
+  		firstName: 'Ruben',
+  		lastName: 'Romero Montes',
+  		email: 'Ruben.ROMERO-MONTES@ec.europa.eu',
+  		mobile: '23423',
+  		employeeNumber: '2343-23423',
+  		validityIdDate: new Date()
+  	};
+
+  	this.submit = function() {
+  		$location.path('/qrcode')
+  	};
+
   });
