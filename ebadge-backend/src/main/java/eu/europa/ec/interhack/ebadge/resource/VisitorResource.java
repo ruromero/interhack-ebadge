8b363dfa997f2eedbc19b798f8d8447e9a186a64
andima <antdim@gmail.com>
2016-10-15 12:21:48 +0200

    renamed visitor handler and merged

--- ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/VisitorResource.java
+++ ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/VisitorResource.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.europa.ec.interhack.ebadge.model.Visitor;
@@ -14,21 +15,28 @@ import eu.europa.ec.interhack.ebadge.repository.VisitorRepository;
  * Created by rromero on 10/10/16.
  */
 @RestController
-public class RegisterVisitor {
+public class VisitorResource {
 
 	@Autowired
 	private VisitorRepository repo;
-	
-	@CrossOrigin(value="*")
-    @RequestMapping(value = "/register", method = RequestMethod.POST, consumes = "application/json")
-    public String register(@RequestBody Visitor visitor) {
-    	
-		// TODO placeholder for validation
-		
-    	// blindly save it to the db for the moment
-    	repo.insert(visitor);
-    	
-        return "registered!";
-    }
 
+	@CrossOrigin(value = "*")
+	@RequestMapping(value = "/register", method = RequestMethod.POST, consumes = "application/json")
+	public String register(@RequestBody Visitor visitor) {
+
+		// TODO placeholder for validation
+
+		// blindly save it to the db for the moment
+		repo.insert(visitor);
+
+		return "registered!";
+	}
+
+	@RequestMapping(value = "/check", method = RequestMethod.POST)
+	public String checkVisitor(@RequestParam(value = "visitor") Visitor visitor) {
+
+		// TODO here check the visitor
+
+		return "approved!";
+	}
 }
