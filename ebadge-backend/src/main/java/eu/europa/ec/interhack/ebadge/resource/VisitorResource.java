e433a7c79ebda14b498293aaee8c37763508ffdc
Daniel Chirita <tnm@thenewsmachine.com>
2016-10-15 17:31:35 +0200

    Double slashes

--- ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/VisitorResource.java
+++ ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/resource/VisitorResource.java
@@ -110,9 +110,9 @@ public class VisitorResource {
 
 		// send email shipping the QR code and PDF
 		String mailBody = String.format(
-				"Dear %s,\\n\\n" + "Please hereby be kindly informed that your access for the visit has been approved.\\n"
-						+ "Find in attachment the QR code with the information of your request for your convenience (needed for the electronic access to the building - using your mobile).\\n"
-						+ "Please find also attached a PDF containing the QR should you need to print it out.\\n\\n" + "Kind regards,\\n" + "The Interhack Team",
+				"Dear %s,\n\n" + "Please hereby be kindly informed that your access for the visit has been approved.\n"
+						+ "Find in attachment the QR code with the information of your request for your convenience (needed for the electronic access to the building - using your mobile).\n"
+						+ "Please find also attached a PDF containing the QR should you need to print it out.\n\n" + "Kind regards,\n" + "The eBadge wizards",
 				String.format("%s %s", visitor.getFirstName(), visitor.getLastName()));
 
 		new MailSender().sendEmail(visitor.getEmail(), "Your eBadge is ready", mailBody, out.getAbsolutePath(), pdfFile.getAbsolutePath());
@@ -135,7 +135,7 @@ public class VisitorResource {
 
 		// Send mail and notify the visitor of rejection
 		String mailSubject = "Your request to visit the European insititutions has been rejected";
-		String mailBody = String.format("Dear %s,\\n\\nYour request to visit %s has been rejected.\\n\\nKind regards,\\nThe eBadge wizards",
+		String mailBody = String.format("Dear %s,\n\nYour request to visit %s has been rejected.\n\nKind regards,\nThe eBadge wizards",
 				String.format("%s %s", visitor.getFirstName(), visitor.getLastName()), visitor.getHost());
 		new MailSender().sendEmail(visitor.getEmail(), mailSubject, mailBody);
 
