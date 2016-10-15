800136dd610310ab5216376215703650cf150c36
Olivier Mungo <olivier.mungo@ec.europa.eu>
2016-10-15 17:33:16 +0200

    no message

---
+++ ebadge-frontend/app/scripts/controllers/scanner.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ebadgeFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ebadgeFrontendApp
+ */
+angular.module('ebadgeFrontendApp')
+  .controller('ScannerCtrl', function () {
+	  window.location.href = 'https://www.the-qrcode-generator.com/scan';
+  });
