cead0faa155ca8eaeabeb44019deb0dd3ac426ad
andima <antdim@gmail.com>
2016-10-15 16:44:34 +0200

    fixed visitor id

---
+++ ebadge-backend/src/main/java/eu/europa/ec/interhack/ebadge/dto/VisitorIdResponse.java
@@ -0,0 +1,14 @@
+package eu.europa.ec.interhack.ebadge.dto;
+
+public class VisitorIdResponse {
+
+	private String visitorId;
+	
+	public void setVisitorId(String visitorId) {
+		this.visitorId = visitorId;
+	}
+	
+	public String getVisitorId() {
+		return visitorId;
+	}
+}
